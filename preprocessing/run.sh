26849d002fec528e2bc289f08912e224b8c60f59
Yeongshin <p17030508@kookmin.ac.kr>
2025-11-02 18:31:43 +0100

    update requirements.txt and run.sh

--- preprocessing/run.sh
+++ preprocessing/run.sh
@@ -1,21 +1,38 @@
 #!/bin/bash
 set -e
-# python -m pip install -U pip
-# pip install -r requirements.txt
 
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+
+# Run All Languages Sequentially
 echo Running preprocessing for Korean
-python preprocessing.py -lang ko -n 1000  
+python preprocessing.py -lang ko -n 10000
 
 echo Running preprocessing for French 
-python preprocessing.py -lang fr -n 1000  
+python preprocessing.py -lang fr -n 10000
 
 echo Running preprocessing for Portuguese
-python preprocessing.py -lang pt -n 1000  
+python preprocessing.py -lang pt -n 10000
 
-echo Running preprocessing for Russian 
-python preprocessing.py -lang ru -n 1000  
+echo Running preprocessing for Spanish
+python preprocessing.py -lang es -n 10000
 
-echo Running preprocessing for Belarusian
-python preprocessing.py -lang be -n 1000 
+echo Running preprocessing for English
+python preprocessing.py -lang en -n 10000
+
+echo Running preprocessing for Tamil
+python preprocessing.py -lang ta -n 10000
+
+echo Running preprocessing for Russian
+python preprocessing.py -lang ru -n 10000
+
+echo Running preprocessing for Belarussian
+python preprocessing.py -lang be -n 10000
+
+echo Running preprocessing for German
+python preprocessing.py -lang ge -n 10000
+
+echo Running preprocessing for Italian
+python preprocessing.py -lang it -n 10000
 
 echo all done
