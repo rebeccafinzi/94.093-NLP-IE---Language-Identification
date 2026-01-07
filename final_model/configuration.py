53cbda22a848d48e23ef411b2e88cd78ae9f3489
Yeongshin <p17030508@kookmin.ac.kr>
2026-01-07 23:50:51 +0100

    update address

--- final_model/configuration.py
+++ final_model/configuration.py
@@ -1,15 +1,12 @@
 # configuration.py
 
-import os
-
 LANG_CODES = ["de", "en", "es", "fr", "it", "ko", "pt", "ta", "be", "ru"]
 MAX_SENTENCES_PER_LANG = 50000
 TEST_RATIO = 0.2
 RANDOM_SEED = 42
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-NLP_PATH = os.path.join(BASE_DIR, "data")
+NLP_PATH= "/content/drive/MyDrive/NLP"
 
-CONLLU_DATA_DIR = os.path.join(BASE_DIR, "preprocessing/data")
-TWITTER_CSV_PATH = os.path.join(NLP_PATH, "tweets_dataset.csv")
-STOPWORDS_PATH = os.path.join(NLP_PATH, "stopwords")
+CONLLU_DATA_DIR = f"{NLP_PATH}/preprocessing/data"
+TWITTER_CSV_PATH = f"{NLP_PATH}/final_solution/data/tweets_dataset.csv"
+STOPWORDS_PATH = f"{NLP_PATH}/final_solution/data/stopwords"
